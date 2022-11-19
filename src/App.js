96d3805b771e40e66b9b853003711d5e8c16dc0b
EmmTarSystem <manu974.training@gmail.com>
2022-11-19 10:25:51 +0100

    avant nouveau system organisme

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import Formulaire from './components/Formulaire';
 import Footer from './components/Footer';
 import './components/app.css';
+
 const App = () => {
-
-
+  
   //Traitement localStorage
   localStorage.setItem("organisme","EDG")
   var organisme = localStorage.getItem("organisme");
 
   console.log(organisme);
 
-
-  return (
-    <div>
-      <Formulaire />
-      <Footer />
-      </div>
     
-  );
-};
+   
+return(
+  <div>
+    <Formulaire />
+    <Footer />
+  </div>
+)
 
+
+}
 export default App;
+        
